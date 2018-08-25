d56d67173d91a00ee227bfb32bda8976b1969634
151250051 <151250051@smail.nju.edu.cn>
2018-08-25 09:42:44 +0800

    首页静态部分展示

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,10 +1,252 @@
 import React,{Component} from 'react'
+import styles from './index.css'
 
 class Index extends Component{
     render(){
         return(
             <div>
-                this is index
+            <div className="igColumns section">
+                <div className="cols-bg white dark-text-theme col-padding col-margin-bot ">
+                    <div className="cols col-444">
+                        <div className="col col1 parsys">
+                            <div className="rich-component section">
+                                <div className="content rich-content">
+                                    <div
+                                        data-background-tablet="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col1/rich_content.adaptive.full.high.jpg/1429286617744.jpg"
+                                        data-background-mobile="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col1/rich_content.adaptive.full.medium.jpg/1429286617744.jpg"
+                                        data-background-img-width="530" data-background-img-height="110"
+                                        data-background-img-resize="true"
+                                        style={{minHeight: '77.41509433962264px', opacity: 1, backgroundImage: 'url(https://a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col1/rich_content.adaptive.full.high.jpg/1429286617744.jpg)'}}
+                                        className="theme text-layout-full-width text-position-top max-width-none imagesize50 content-no-margin crop-center js_clickable "
+                                        data-background-desktop-used="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col1/rich_content.adaptive.full.high.jpg/1429286617744.jpg">
+                                        <div className="text text-content">
+                                            <p>&nbsp;</p>
+                                            <p>&nbsp;</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="list parbase section">
+                                <div
+                                    className="list-wrapper js_list clearfix style-key-benefits none-bg dark-text-theme">
+                                    <h2 className="heading h2"> 成功交易，明智选择 </h2>
+                                    <div className="description-heading"><p><strong>您的优势首选</strong></p>
+                                    </div>
+                                    <ul className="list-container clearfix list-vertical cols-1" data-cols="1">
+                                        <li value="1" className="item1">
+                                            <div className="inner">
+                                                <h3>业界领先</h3>
+                                                <div className="desc"><p>自1974年以来，我们一直深受信任，享誉全球</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li value="2" className="item2">
+                                            <div className="inner">
+                                                <h3>更多交易，更低收费</h3>
+                                                <div className="desc"><p>窄幅价差，低额点差，更有超过15,000市场可交易</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li value="3" className="item3">
+                                            <div className="inner">
+                                                <h3>优越平台</h3>
+                                                <div className="desc"><p>快速、可信、强大的交易平台，让您的交易策略执行如虎添翼。</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col2 parsys">
+                            <div className="rich-component section">
+                                <div className="content rich-content">
+                                    <div
+                                        data-background-tablet="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col2/rich_content.adaptive.full.high.jpg/1429286618116.jpg"
+                                        data-background-mobile="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col2/rich_content.adaptive.full.medium.jpg/1429286618116.jpg"
+                                        data-background-img-width="530" data-background-img-height="110"
+                                        data-background-img-resize="true"
+                                        style={{minHeight: '77.41509433962264px', opacity: 1, backgroundImage: 'url(https://a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col2/rich_content.adaptive.full.high.jpg/1429286618116.jpg)'}}
+                                        className="theme text-layout-full-width text-position-top max-width-none imagesize50 content-no-margin crop-center js_clickable "
+                                        data-background-desktop-used="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col2/rich_content.adaptive.full.high.jpg/1429286618116.jpg">
+                                        <div className="text text-content">
+                                            <p>&nbsp;</p>
+                                            <p>&nbsp;</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="list parbase section">
+                                <div className="list-wrapper js_list clearfix style-key-benefits bg-none ">
+                                    <h2 className="heading h2"> 更快交易，立即交易 </h2>
+                                    <div className="description-heading"><p>
+                                        <strong>经过倾心设计，我们的平台是最快速、最可靠，也是最有效率的交易平台。</strong></p>
+                                    </div>
+                                    <ul className="list-container clearfix list-vertical cols-1" data-cols="1">
+                                        <li value="1" className="item1">
+                                            <div className="inner">
+                                                <h3>简洁介面</h3>
+                                                <div className="desc"><p>经典“交易”介面</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li value="2" className="item2">
+                                            <div className="inner">
+                                                <h3>精益求精</h3>
+                                                <div className="desc"><p>定期推出更新以及全新功能</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li value="3" className="item3">
+                                            <div className="inner">
+                                                <h3>更优价格</h3>
+                                                <div className="desc"><p>众多数据来源中精选最优价格</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col3 parsys">
+                            <div className="rich-component section">
+                                <div className="content rich-content">
+                                    <div
+                                        data-background-tablet="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col3/rich_content.adaptive.full.high.jpg/1429286618323.jpg"
+                                        data-background-mobile="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col3/rich_content.adaptive.full.medium.jpg/1429286618323.jpg"
+                                        data-background-img-width="530" data-background-img-height="110"
+                                        data-background-img-resize="true"
+                                        style={{minHeight: '77.41509433962264px', opacity: 1, backgroundImage: 'url(https://a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col3/rich_content.adaptive.full.high.jpg/1429286618323.jpg)'}}
+                                        className="theme text-layout-full-width text-position-top max-width-none imagesize50 content-no-margin crop-right js_clickable "
+                                        data-background-desktop-used="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_0/col%20col3/rich_content.adaptive.full.high.jpg/1429286618323.jpg">
+                                        <div className="text text-content">
+                                            <p>&nbsp;</p>
+                                            <p>&nbsp;</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="list parbase section">
+                                <div
+                                    className="list-wrapper js_list clearfix style-key-benefits none-bg dark-text-theme">
+                                    <h2 className="heading h2"> 专家透视，专业支援 </h2>
+                                    <div className="description-heading"><p><strong>借力我们的交易工具、资源和经验帮助您提升交易优势</strong>
+                                    </p>
+                                    </div>
+                                    <ul className="list-container clearfix list-vertical cols-1" data-cols="1">
+                                        <li value="1" className="item1">
+                                            <div className="inner">
+                                                <h3>客户培训</h3>
+                                                <div className="desc"><p>我们为您提供交易文章和指南，以及一系列培训网络培训资料。</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li value="2" className="item2">
+                                            <div className="inner">
+                                                <h3>丰富工具和海量数据</h3>
+                                                <div className="desc"><p>所有主要市场的免费数据、财经日历、市场分析评论以及更多支援。</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li value="3" className="item3">
+                                            <div className="inner">
+                                                <h3>专业支援</h3>
+                                                <div className="desc"><p>态度诚恳、知识丰富的本地服务团队为您提供24小时支持。</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className = "rich-component section" >
+            <div className = "content rich-content" >
+            <div className = "theme text-layout-full-width text-position-top max-width-none text-no-image imagesize50 js_clickable " >
+            <div className = "text text-content" >
+            <h2 style = {{textAlign: 'center'}}> 开设交易仅需 3, 2, 1...</h2>
+                <h3 style={{textAlign: 'center'}}>开设账户无最低存款要求，
+                    您可在数分钟内开始进行差价合约交易。</h3>
+    </div>
+    </div>
+    </div>
+
+        <div className="igColumns section">
+            <div className="cols-bg white dark-text-theme col-padding col-no-margin ">
+                <div className="cols col-444">
+                    <div className="col col1 parsys">
+                        <div className="promo-component section">
+                            <div className="content promo">
+                                <div
+                                    className="theme white-opaque text-bottom full-width text-position-top max-width-none imagesize50 crop-center js_clickable  clickable">
+                                    <div className="image text-image">
+<span data-link="" data-classnames="" data-alt="" style={{paddingBottom: '22.520107%'}}
+      className="lazyload adaptive-image white-opaque max-width-none" data-picture="">
+<span
+    data-src="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_1/col%20col1/promo.adaptive.full.high.png/1442569085334.png"
+    data-width="373" data-height="84" data-media="(min-width: 1025px)"><img alt=""
+                                                                            src="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_1/col%20col1/promo.adaptive.full.high.png/1442569085334.png" /></span>
+</span>
+                                    </div>
+                                    <div className="text text-content">
+                                        <h3 style={{textAlign: 'center'}}>填写表格</h3>
+                                        <p className="text-col-4">通常我们可在短时间内为您验证身份，您即可在数分钟内登入您的交易账户。</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col col2 parsys">
+                        <div className="promo-component section">
+                            <div className="content promo">
+                                <div
+                                    className="theme white-opaque text-bottom full-width text-position-top max-width-none imagesize50 crop-center js_clickable  clickable">
+                                    <div className="image text-image">
+<span data-link="" data-classnames="" data-alt="" style={{paddingBottom: '22.520107%'}}
+      className="lazyload adaptive-image white-opaque max-width-none" data-picture="">
+<span
+    data-src="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_1/col%20col2/promo.adaptive.full.high.png/1442569085219.png"
+    data-width="373" data-height="84" data-media="(min-width: 1025px)"><img alt=""
+                                                                            src="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_1/col%20col2/promo.adaptive.full.high.png/1442569085219.png" /></span>
+</span>
+                                    </div>
+                                    <div className="text text-content">
+                                        <h3 style={{textAlign: 'center'}}>账户注资</h3>
+                                        <p className="text-col-4">透过信用卡/储蓄卡可即时存款，也可使用银行转账</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col col3 parsys">
+                        <div className="promo-component section">
+                            <div className="content promo">
+                                <div
+                                    className="theme white-opaque text-bottom full-width text-position-top max-width-none imagesize50 crop-center js_clickable  clickable">
+                                    <div className="image text-image">
+<span data-link="" data-classnames="" data-alt="" style={{paddingBottom: '22.520107%'}}
+      className="lazyload adaptive-image white-opaque max-width-none" data-picture="">
+<span data-src="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_1/col%20col3/promo.adaptive.full.high.png/1442569085123.png"
+    data-width="373" data-height="84" data-media="(min-width: 1025px)"><img alt=""
+                                                                            src="//a.c-dn.net/c/content/igcom/zh_CN/_jcr_content/content-parsys/columns_1/col%20col3/promo.adaptive.full.high.png/1442569085123.png" />
+</span>
+</span>
+                                    </div>
+                                    <div className="text text-content">
+                                        <h3 style={{textAlign: 'center'}}>进行首笔交易</h3>
+                                        <p className="text-col-4">透过我们的交易工具发掘交易机会，定单在数秒内即可完成。</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            </div>
             </div>
         )
     }
